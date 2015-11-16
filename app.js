7b7b0f2469e402ce3d8494cfe3051fe3f0824efe
daecheol <kdc24@naver.com>
2015-11-16 15:48:27 +0900

    layout

--- app.js
+++ app.js
@@ -5,14 +5,24 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+
+
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var mongoose   = require('mongoose');
 
 var app = express();
 
 // view engine setup//
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// mongodb connectd
+// 아래 DB접속 주소는 꼭 자기 것으로 바꾸세요!
+mongoose.connect('mongodb://dckang:dc0413ky@ds045064.mongolab.com:45064/webp');
+mongoose.connection.on('error', console.log);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,6 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components',  express.static(path.join(__dirname, '/bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
